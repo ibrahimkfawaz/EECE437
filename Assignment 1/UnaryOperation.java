131f3031283871d4cf121b35cdbccff6a0320fff
Ibrahim Fawaz <Ibrahim Fawaz>
2017-02-18 21:08:20 +0200

    UnaryOperations

--- Assignment 1/UnaryOperation.java
+++ Assignment 1/UnaryOperation.java
@@ -24,16 +24,15 @@ class TANot extends UnaryOperation
 			System.out.println(this.getName());
 		}
 	}
+	public TANot(String val)
+	{
+		super.setName(val);
+	}
 	public TANot(TABool val)
 	{
 		operand = val;
 		result = new TABool(!(val.getValue()));
 	}
-	public TANot(String val)
-	{
-		super.setName(val);
-	}
-	
 	public TANot(UnaryOperation val)
 	{
 		operand = val;
@@ -52,7 +51,39 @@ class TANot extends UnaryOperation
 			result = new TABool(!(temp.getValue()));
 		}
 	}
-
+	public void evaluate()
+	{
+		if(operand instanceof TABool)
+		{
+			TABool temp = (TABool)operand;
+			result = new TABool(!(temp.getValue()));
+		}
+		else if(operand instanceof UnaryOperation)
+		{
+			UnaryOperation temp = (UnaryOperation)operand;
+			if(temp.result instanceof TABool)
+			{
+				TABool temp1 = (TABool) temp.result;
+				result = new TABool(!(temp1.getValue()));
+			}
+			
+		}
+		else if(operand instanceof BinaryOperation)
+		{
+			BinaryOperation temp = (BinaryOperation)operand;
+			if(temp.result instanceof TABool)
+			{
+				TABool temp1 = (TABool) temp.result;
+				result = new TABool(!(temp1.getValue()));
+			}
+			
+		}
+	}
+	public void printState()
+	{
+		TABool temp = (TABool)result;
+		System.out.println(temp.getValue());
+	}
 }
 
 class TAUnaryMinus extends UnaryOperation
@@ -112,6 +143,62 @@ class TAUnaryMinus extends UnaryOperation
 		{
 			TADouble temp = (TADouble) val.result;
 			result = new TADouble(temp.getValue());
+		}
+	}
+	public void evaluate()
+	{
+		if(operand instanceof TAInt)
+		{
+			TAInt temp = (TAInt)operand;
+			result = new TAInt(temp.getValue()*-1);
+		}
+		else if(operand instanceof TADouble)
+		{
+			TADouble temp = (TADouble)operand;
+			result = new TADouble(temp.getValue()*-1);
+		}
+		else if(operand instanceof UnaryOperation)
+		{
+			UnaryOperation temp = (UnaryOperation)operand;
+			if(temp.result instanceof TAInt)
+			{
+				TAInt temp1 = (TAInt) temp.result;
+				result = new TAInt(temp1.getValue()*-1);
+			} 
+			else if(temp.result instanceof TADouble)
+			{
+				TADouble temp1 = (TADouble) temp.result;
+				result = new TADouble(temp1.getValue()*-1);
+			}
+			
+		}
+		else if(operand instanceof BinaryOperation)
+		{
+			
+			BinaryOperation temp = (BinaryOperation)operand;
+			if(temp.result instanceof TAInt)
+			{	
+				TAInt temp1 = (TAInt) temp.result;
+				result = new TAInt(temp1.getValue()*-1);
+			} 
+			else if(temp.result instanceof TADouble)
+			{
+				TADouble temp1 = (TADouble) temp.result;
+				result = new TADouble(temp1.getValue()*-1);
+			}
+		}
+	}
+	public void printState()
+	{
+		if(result instanceof TAInt)
+		{
+			TAInt temp = (TAInt)result;
+			System.out.println(temp.getValue());
+		}
+		else if(result instanceof TADouble)
+		{
+			TADouble temp = (TADouble)result;
+			System.out.println(temp.getValue());
 		}
 	}
 }
@@ -175,6 +262,62 @@ class TACeiling extends UnaryOperation
 			result = new TADouble(Math.ceil(temp.getValue()));
 		}
 	}
+	public void evaluate()
+	{
+		if(operand instanceof TAInt)
+		{
+			TAInt temp = (TAInt)operand;
+			result = new TAInt(temp.getValue());
+		}
+		else if(operand instanceof TADouble)
+		{
+			TADouble temp = (TADouble)operand;
+			result = new TADouble(Math.ceil(temp.getValue()));
+		}
+		else if(operand instanceof UnaryOperation)
+		{
+			UnaryOperation temp = (UnaryOperation)operand;
+			if(temp.result instanceof TAInt)
+			{
+				TAInt temp1 = (TAInt) temp.result;
+				result = new TAInt(temp1.getValue());
+			} 
+			else if(temp.result instanceof TADouble)
+			{
+				TADouble temp1 = (TADouble) temp.result;
+				result = new TADouble(Math.ceil(temp1.getValue()));
+			}
+			
+		}
+		else if(operand instanceof BinaryOperation)
+		{
+			
+			BinaryOperation temp = (BinaryOperation)operand;
+			if(temp.result instanceof TAInt)
+			{	
+				TAInt temp1 = (TAInt) temp.result;
+				result = new TAInt(temp1.getValue());
+			} 
+			else if(temp.result instanceof TADouble)
+			{
+				TADouble temp1 = (TADouble) temp.result;
+				result = new TADouble(Math.ceil(temp1.getValue()));
+			}
+		}
+	}
+	public void printState()
+	{
+		if(result instanceof TAInt)
+		{
+			TAInt temp = (TAInt)result;
+			System.out.println(temp.getValue());
+		}
+		else if(result instanceof TADouble)
+		{
+			TADouble temp = (TADouble)result;
+			System.out.println(temp.getValue());
+		}
+	}
 }
 
 class TAFloor extends UnaryOperation
@@ -234,6 +377,62 @@ class TAFloor extends UnaryOperation
 		{
 			TADouble temp = (TADouble) val.result;
 			result = new TADouble(Math.floor(temp.getValue()));
+		}
+	}
+	public void evaluate()
+	{
+		if(operand instanceof TAInt)
+		{
+			TAInt temp = (TAInt)operand;
+			result = new TAInt(temp.getValue());
+		}
+		else if(operand instanceof TADouble)
+		{
+			TADouble temp = (TADouble)operand;
+			result = new TADouble(Math.floor(temp.getValue()));
+		}
+		else if(operand instanceof UnaryOperation)
+		{
+			UnaryOperation temp = (UnaryOperation)operand;
+			if(temp.result instanceof TAInt)
+			{
+				TAInt temp1 = (TAInt) temp.result;
+				result = new TAInt(temp1.getValue());
+			} 
+			else if(temp.result instanceof TADouble)
+			{
+				TADouble temp1 = (TADouble) temp.result;
+				result = new TADouble(Math.floor(temp1.getValue()));
+			}
+			
+		}
+		else if(operand instanceof BinaryOperation)
+		{
+			
+			BinaryOperation temp = (BinaryOperation)operand;
+			if(temp.result instanceof TAInt)
+			{	
+				TAInt temp1 = (TAInt) temp.result;
+				result = new TAInt(temp1.getValue());
+			} 
+			else if(temp.result instanceof TADouble)
+			{
+				TADouble temp1 = (TADouble) temp.result;
+				result = new TADouble(Math.floor(temp1.getValue()));
+			}
+		}
+	}
+	public void printState()
+	{
+		if(result instanceof TAInt)
+		{
+			TAInt temp = (TAInt)result;
+			System.out.println(temp.getValue());
+		}
+		else if(result instanceof TADouble)
+		{
+			TADouble temp = (TADouble)result;
+			System.out.println(temp.getValue());
 		}
 	}
 }
