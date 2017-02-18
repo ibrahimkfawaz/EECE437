798d367ec6dff51b99b2d7952933f41009c7339e
Ibrahim Fawaz <Ibrahim Fawaz>
2017-02-18 12:55:37 +0200

    Test file

--- Assignment 1/Test.java
+++ Assignment 1/Test.java
@@ -1,13 +1,14 @@
 public class Test
 {
-	public static void Main(String []args)
+	public static void main(String []args)
 	{
-		TAInt x = new TAInt(9);
-		TADouble d = new TADouble(5.6);
-		TADouble dd = new TADouble(2.73);
-		TACeiling y = new TACeiling(d);
-		TABool b = new TABool(True);
-		TALessThan ta = new TALessThan(d,dd);
+		TAInt x = new TAInt(7);
+		TADouble d = new TADouble(2.3);
+		TACeiling y = new TACeiling(d); //y is ceiling of d, y=3
+		TABool b = new TABool(); //default constructor, value is true
+		TALessThan t1 = new TALessThan(x,y); // 7<3, gives false
+		TAAnd t2 = new TAAnd(t1,b); //true AND false = false
+		TABool temp = (TABool)t2.result;
+		System.out.println(temp.getValue()); //prints the value, false
 	}
 }
-
