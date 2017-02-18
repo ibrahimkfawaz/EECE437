677196b8ee3aa8a212d4a6782acd26442f6e8f27
Ibrahim Fawaz <Ibrahim Fawaz>
2017-02-18 10:11:18 +0200

    Fixed bugs

--- Assignment 1/BinaryOperation.java
+++ Assignment 1/BinaryOperation.java
@@ -260,6 +260,70 @@ class TAAdd extends BinaryOperation
 	{
 		super.setName(a);
 	}
+	public TAAdd(UnaryOperation op1,TAInt op2)
+	{
+		if(op1.result instanceof TAInt)
+		{
+			TAInt temp = (TAInt)op1.result;
+			result = new TAInt(temp.getValue()+op2.getValue());
+		}
+	}
+	public TAAdd(UnaryOperation op1,TADouble op2)
+	{
+		if(op1.result instanceof TADouble)
+		{
+			TADouble temp = (TADouble)op1.result;
+			result = new TADouble(temp.getValue()+op2.getValue());
+		}
+	}
+	public TAAdd(UnaryOperation op1,UnaryOperation op2)
+	{
+		if(op1.result instanceof TAInt && op2.result instanceof TAInt)
+		{
+			TAInt temp1 = (TAInt)op1.result;
+			TAInt temp2 = (TAInt)op2.result;
+			result = new TAInt(temp1.getValue() + temp2.getValue());
+		}
+		else if(op1.result instanceof TADouble && op2.result instanceof TADouble)
+		{
+			TADouble temp1 = (TADouble)op1.result;
+			TADouble temp2 = (TADouble)op2.result;
+			result = new TADouble(temp1.getValue() + temp2.getValue());
+			
+		}
+	}
+	public TAAdd(BinaryOperation op1, TAInt op2)
+	{
+		if(op1.result instanceof TAInt)
+		{
+			TAInt temp = (TAInt)op1.result;
+			result = new TAInt(temp.getValue()+op2.getValue());
+		}
+	}
+	public TAAdd(BinaryOperation op1,TADouble op2)
+	{
+		if(op1.result instanceof TADouble)
+		{
+			TADouble temp = (TADouble)op1.result;
+			result = new TADouble(temp.getValue()+op2.getValue());
+		}
+	}
+	public TAAdd(BinaryOperation op1,BinaryOperation op2)
+	{
+		if(op1.result instanceof TAInt && op2.result instanceof TAInt)
+		{
+			TAInt temp1 = (TAInt)op1.result;
+			TAInt temp2 = (TAInt)op2.result;
+			result = new TAInt(temp1.getValue() + temp2.getValue());
+		}
+		else if(op1.result instanceof TADouble && op2.result instanceof TADouble)
+		{
+			TADouble temp1 = (TADouble)op1.result;
+			TADouble temp2 = (TADouble)op2.result;
+			result = new TADouble(temp1.getValue() + temp2.getValue());
+			
+		}
+	}
 }
 
 class TASubtract extends BinaryOperation
