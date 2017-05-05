a2b9dd4162194069d7635df3c5b7980e7adb3e64
Ibrahim Fawaz <Ibrahim Fawaz>
2017-05-05 23:47:02 +0300

    value

--- Assignment 2/TAValue.java
+++ Assignment 2/TAValue.java
@@ -1,4 +1,63 @@
 public class TAValue extends TAObject
 {
-	//TAValue represents the actual value of a Term that is what it actually holds
+	//TAValue represents the actual value of a Term i.e what it actually holds
+		
+	public boolean isPair()
+	{
+		return false;
+	}
+	public boolean isArray()
+	{
+		return false;
+	} 	
+	public boolean isDouble()
+	{
+		return false;
+	}
+	public boolean isInt()
+	{
+		return false;
+	}
+	public boolean isBool()
+	{
+		return false;
+	}
+
+	public int getInt()
+	{
+		return 0;
+	}
+	public double getDouble()
+	{
+		return 0.0;
+	}
+	public boolean getBool()
+	{
+		return false;
+	}
+	public TAValue getArray()
+	{
+
+	}
+	public TAValue getPair()
+	{
+		
+	}
+
+	public void assignMinus(TAValue op) {}
+	public void assignNot(TAValue op) {}
+	public void assignCeiling(TAValue op){ }
+	public void assignFloor(TAValue op){ }
+	public void assignPlus(TAValue op1, TAValue op2) { }
+  	public void assignSubtract(TAValue op1, TAValue op2) {  }
+ 	public void assignProduct(TAValue op1, TAValue op2) {  }
+ 	public void assignDivision(TAValue op1, TAValue op2) {  }
+ 	public void assignLessThan(TAValue op1, TAValue op2) {}
+  	public void assignEqual(TAValue op1, TAValue op2) {}
+  	public void assignGreaterThan(TAValue op1, TAValue op2) {}
+  	public void assignLAnd(TAValue op1, TAValue op2) { }
+ 	public void assignLOr(TAValue op1, TAValue op2) { }
+ 	public void assignEquiv(TAValue op1, TAValue op2) { }
+ 	public void assignImplies(TAValue op1, TAValue op2) { }
+		
 }
