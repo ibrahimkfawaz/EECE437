842cb80cf9fa256af7209bf9eab3a03e80aad0a0
Ibrahim Fawaz <Ibrahim Fawaz>
2017-05-09 23:20:53 +0300

    statements

--- Assign2_C++/Additions/TAStmt.h
+++ Assign2_C++/Additions/TAStmt.h
@@ -3,7 +3,11 @@
 //statement
 struct TAStmt : public TAObj
 {
-	virtual void list()=0;
+	virtual void list(ostream &os)=0;
+	virtual void list()
+	{
+		list(cout);
+	}
 	virtual void evaluate()=0;
 };
 
@@ -12,4 +16,50 @@ struct TAAtomicStmt : public TAStmt
 
 };
 
-struct 
+struct TAAssignment : public TAAtomicStmt
+{
+	TATerm & target;
+	TAExpression & expression;
+
+	TAAssignment(TATerm & t,TAExpression & e) : target(t), expression(e)
+	{
+		//TODO: check types, see if they match else throw exception
+	}
+
+	virtual void evaluate()
+	{
+		TAValue & temp;
+		temp = expression.evaluate();
+		target.set(temp);
+	}
+
+	virtual void list(ostream &os)
+	{
+		os << "( ";
+		expression.list(os);
+		os << " is assigned to ";
+		target.list(os);
+		os << " )" <<endl;
+
+	}
+};
+
+struct TACondition : public TAAtomicStmt
+{
+	
+};
+
+struct TALoop : public TAAtomicStmt
+{
+	
+};
+
+struct TAEmptyStmt : public TAStmt
+{
+	
+};
+
+struct  TASequential : public TAStmt
+{
+	
+};
